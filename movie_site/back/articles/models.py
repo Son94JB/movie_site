c24faa64a78c0d8fdc508ada62ea66654712bfdf
Son <thswnsqo99@gmail.com>
2023-05-18 17:33:24 +0900

    20230518 vue working(layout)

--- movie_site/back/articles/models.py
+++ movie_site/back/articles/models.py
@@ -5,19 +5,20 @@ from django.conf import settings
 class Article(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    article_likes = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='ArticleLike')
+    article_likes = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_article')
 
 class Comment(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
-    username = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    comment_likes = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='CommentLike')
+    comment_likes = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_comment')
 
 class CommentLike(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
