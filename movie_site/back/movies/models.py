5c49d810e76afea941a069e32c3f9816a325aa4d
Son <thswnsqo99@gmail.com>
2023-05-18 15:33:04 +0900

    follow like added

--- movie_site/back/movies/models.py
+++ movie_site/back/movies/models.py
@@ -45,14 +45,4 @@ class MovieReview(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='reviews')
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='moviereviews')
 
-# 영화 상세정보 모델
-class MovieDetail(models.Model):
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
-    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
-    director = models.ForeignKey(Director, on_delete=models.CASCADE)
-    # poster 필드 추가, 이미지 파일을 저장할 수 있는 FileField 사용
-    poster = models.FileField(blank=True, upload_to='posters/')
-    # trailer 필드 추가, url 링크를 저장할 수 있는 URLField 사용
-    trailer = models.URLField(blank=True)
-    moviereview = models.ForeignKey(MovieReview, on_delete=models.CASCADE, related_name='moviedetails')
     
